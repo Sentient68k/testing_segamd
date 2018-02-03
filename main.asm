1b9f47a6191669588d68884fd2ced92d08f07dbb
Mike C <mjcorm@gmail.com>
2018-02-03 00:40:58 -0800

    Lots of stuff
    Loads of changes. Working through the basics quickly.

--- main.asm
+++ main.asm
@@ -1,14 +1,45 @@
 ; Main program for the ROM.
 __main:
-	move.w	#$8F02, vdp_control	; Autoincrement to 2 bytes on VDP
-	
-	; Move palettes to CRAM
-	move.l	#vdp_write_palettes, vdp_control	; Setup VDP to write CRAM to address $0000
-	lea	Palettes, a0	; Load address of palettes into a0
-	move.l	#$1F, d0	; 128 bytes of data (4 palettes, 32 longs, -1 for counter)
+	lea	Palette01, a0	; Load address of palettes into a0
+	jsr LoadPalette ; Jump to Palette Loading subroutine.
 
-	@ColorLoop:
-	move.l (a0)+, vdp_data 	; Move data to VDP port, increment address pointer to move up in palette lines
-	dbra d0, @ColorLoop
+	lea test_tiles_01, a0 ; Get tiles address ready for loading.
+	move.l #test_tiles_01VRAM, d0
+	move.l #test_tiles_01SizeT, d1
+	jsr LoadTiles
 
 	jmp __main
+
+LoadPalette:
+    ; a0 - Address of palettes to load.
+    move.w #$8F02, vdp_control ; Set VDP address autoincrement to 2 bytes
+
+    ; Move selected pallet to CRAM
+    move.l #vdp_write_palettes, vdp_control ; Setup VDP data port to load into CRAM
+    move.l #$1F, d0 ; ; 128 bytes of data (4 palettes, 32 longs, -1 for counter)
+
+    @ColorLoop:
+    move.l (a0)+, vdp_data ; Move data to VDP Port, increment address
+    dbra d0, @ColorLoop
+
+    rts
+
+LoadTiles:
+; a0 - Tiles address (l)
+; d0 - VRAM address	(w)
+; d1 - Num chars (w)
+
+	swap	d0	;Shift VRAM addr to upper word
+	add.l 	#vdp_write_tiles, d0 	;VRAM write cmd + VRAM dest addr
+	move.l 	d0, vdp_control		;Send address to VDP cmd port
+
+	subq.b 	#$1, d1		; Num of chars -1
+	@CharCopy:
+	move.w 	#$07, d2 	; 8 longwords in tile
+	@LongCopy:
+	move.l 	(a0)+, vdp_data 	;Copy one line of tile to VDP data port
+	dbra 	d2, @LongCopy
+	dbra 	d1, @CharCopy
+
+	rts
+
--- program.asm
+++ program.asm
@@ -1,6 +1,7 @@
 ; Main assembly file that collects all others in proper order.
-	include 'init.asm'
-	include	'main.asm'
-	include	'palettes.asm'
-	include	'GLOBALS.asm'
-__end	; Address for end of ROM.
+    include 'init.asm'
+    include 'main.asm'
+    include 'palettes.asm'
+    include './data/test_tiles_01.asm'
+    include 'GLOBALS.asm'
+__end   ; Address for end of ROM.
